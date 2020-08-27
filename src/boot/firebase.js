28bee66c22c6bca0bdd0b74e0b2fb5a74516f739
Daniel Caria <daniel.caria@yahoo.com.br>
2020-08-27 08:45:17 -0300

    Update firebase.js

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -7,16 +7,6 @@ import 'firebase/database'
 // "async" is optional
 export default async ({ Vue }) => {
   
-  let firebaseConfig = {
-    apiKey: "AIzaSyAstQdq1CNDNZX-O-voCOadfXUbkJcnkpo",
-    authDomain: "clinica-c64b5.firebaseapp.com",
-    databaseURL: "https://clinica-c64b5.firebaseio.com",
-    projectId: "clinica-c64b5",
-    storageBucket: "clinica-c64b5.appspot.com",
-    messagingSenderId: "726394542061",
-    appId: "1:726394542061:web:836d7e6213b8f2379c79e4",
-    measurementId: "G-T1EDQHP7ED"
-  }
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig)
