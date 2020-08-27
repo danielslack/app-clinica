2de46e3bd992d223586c05406c74e7a63e193f48
Daniel Caria <daniel.slack2@gmail.com>
2020-08-27 08:47:00 -0300

    Ajuste config firebase
    Ajuste config firebase

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,21 +3,13 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
+import  { firebaseConfig } from './config'
+
 
 // "async" is optional
 export default async ({ Vue }) => {
   
-  let firebaseConfig = {
-    apiKey: "AIzaSyAstQdq1CNDNZX-O-voCOadfXUbkJcnkpo",
-    authDomain: "clinica-c64b5.firebaseapp.com",
-    databaseURL: "https://clinica-c64b5.firebaseio.com",
-    projectId: "clinica-c64b5",
-    storageBucket: "clinica-c64b5.appspot.com",
-    messagingSenderId: "726394542061",
-    appId: "1:726394542061:web:836d7e6213b8f2379c79e4",
-    measurementId: "G-T1EDQHP7ED"
-  }
-
+ 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig)
 
